a214997010e139635fa9f1fa64a58cd87ab99580
Daniel Ritchie <ritchieface@gmail.com>
2021-05-15 20:53:38 -0600

    corrected dir

--- utils/backup_wallet.sh
+++ utils/backup_wallet.sh
@@ -8,9 +8,9 @@ if (( $appendage == "" )); then
 fi
 
 #copy the wallet so we don't interrupt anything
-cp ~/.chia/mainnet/wallet/blockchain_wallet_v1_mainnet_3782091020.sqlite ~/.chia/mainnet/wallet/blockchain_wallet_v1_mainnet_3782091020.sqlite_$appendage
+cp ~/.chia/mainnet/wallet/db/blockchain_wallet_v1_mainnet_3782091020.sqlite ~/.chia/mainnet/wallet/blockchain_wallet_v1_mainnet_3782091020.sqlite_$appendage
 
 #backup to s3
-aws s3 mv ~/.chia/mainnet/wallet/blockchain_wallet_v1_mainnet_3782091020.sqlite_$appendage s3://lotsoplots
+aws s3 mv ~/.chia/mainnet/wallet/db/blockchain_wallet_v1_mainnet_3782091020.sqlite_$appendage s3://lotsoplots
 
 
