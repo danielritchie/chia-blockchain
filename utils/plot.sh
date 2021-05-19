532fe0b19a5aac0546f895f64a513ba245ca2f35
Daniel Ritchie <ritchieface@gmail.com>
2021-05-18 23:05:36 -0600

    new plot script

--- utils/plot.sh
+++ utils/plot.sh
@@ -30,8 +30,6 @@ while [[ $CURRENT_PLOT -lt $PLOTS_IN_PARALLEL ]]; do
 	CURRENT_PLOT=$CURRENT_PLOT + 1
 	
 	echo "Current Plot: $CURRENT_PLOT of $PLOTS_IN_PARALLEL"
-	echo "Waiting $PLOT_STAGGER_TIME minutes to start plotting..."
-	sleep "${PLOT_STAGGER_TIME}m"
 		
 	DATE_PREFIX=`date '+%Y-%m-%d_%H-%M-%S-%N'`
 	PLOT_LOGFILE="${OUTPUT_DIR}/plotlogs/${DATE_PREFIX}_${INSTANCE_ID}"
@@ -58,10 +56,13 @@ while [[ $CURRENT_PLOT -lt $PLOTS_IN_PARALLEL ]]; do
 
 	" > $PLOT_LOGFILE
 
-	time chia plots create -k 32 -b $PLOT_BYTES -r $PLOT_THREADS -u 128 -n 1 -t ${TMP_DIR} -d ${OUTPUT_DIR} -f a4ab1f8213ebc06cbce49bf56b923467cf3263fdf587efff5ef0b71635f7c5b51d239046586354db24dfab188071d978 -p 96cbc06eb710bb3c43e8a64208dd49bce37cf440ca1c2bd6ee3c99124818c8ad0520d0b3f401151148a3a0e3c97f9f03;echo "**************** PLOTTING COMPLETE ****************" >> $PLOT_LOGFILE;aws s3 mv ${OUTPUT_DIR} s3://lotsoplots --recursive --exclude "*" --include "*.plot" --exclude "*/*"; aws s3 cp $PLOT_LOGFILE s3:/lotsoplots/plotlogs/
+	# Run the whole thing in the background (has to be a better way!)
+	{time chia plots create -k 32 -b $PLOT_BYTES -r $PLOT_THREADS -u 128 -n 1 -t ${TMP_DIR} -d ${OUTPUT_DIR} -f a4ab1f8213ebc06cbce49bf56b923467cf3263fdf587efff5ef0b71635f7c5b51d239046586354db24dfab188071d978 -p 96cbc06eb710bb3c43e8a64208dd49bce37cf440ca1c2bd6ee3c99124818c8ad0520d0b3f401151148a3a0e3c97f9f03;echo "**************** PLOTTING COMPLETE ****************" >> $PLOT_LOGFILE ;aws s3 mv ${OUTPUT_DIR} s3://lotsoplots --recursive --exclude "*" --include "*.plot" --exclude "*/*"; aws s3 cp $PLOT_LOGFILE s3:/lotsoplots/plotlogs/} &
 	
 	
-	echo "Finished with: $CURRENT_PLOT"
+	echo "Finished starting: $CURRENT_PLOT"
+	echo "Waiting $PLOT_STAGGER_TIME minutes before starting next plot..."
+	sleep "${PLOT_STAGGER_TIME}m"
 done
 
 echo "
