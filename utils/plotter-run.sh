9a71eb98fe4eec14a21011b5a7a02c6cd1688e6f
Daniel Ritchie <ritchieface@gmail.com>
2021-05-22 10:32:58 -0600

    updated schedule

--- utils/plotter-run.sh
+++ utils/plotter-run.sh
@@ -1,97 +1,101 @@
-######################################################
-### PLOTTER SETUP
-######################################################
-#
-#IAM Role
-#Security Group 
-#######################################################
+#!/bin/bash
+
+#CPU / Scheduling notes
+#at 38x4 seems to be pegged CPU BUT also some blips of freedom and underutilized disk
+#... may actually be OK since there will be heavy writes when the other ~10+ phases are going
+# CPU pegged disk underutilized (have changed config to give first passess of ph1 some breathing room)
 
 
+# INSTALL CHIA
+sudo apt-get update
+sudo apt-get upgrade -y
 
-exit "not ready for prime time... need to cleanup and new AMI before running"
+# Install Git
+sudo apt install git -y
 
-# Setup
-#sudo apt install awscli -y
-#sudo mkdir /tmpdisk
-#sudo chmod 777 /tmpdisk
-#sudo vi /etc/fstab
+# Checkout the source and install
+git clone https://github.com/danielritchie/chia-blockchain.git -b latest --recurse-submodules
+cd chia-blockchain
 
-	#for h1.2xlarge: /dev/xvdb       /tmpdisk        auto    defaults,nofail,x-systemd.requires=cloud-init.service,comment=cloudconfig       0       2
-	
-	#for m5n:
-	#/dev/nvme1n1 /tmpdisk              ext4    defaults                0 0
+sh install.sh
 
-
-#TBD other fs types (theoretically btrfs looks like best balance of quality/speed, also need to check plots from it)
-sudo mkfs -t btrfs /dev/xvdc
-#sudo cfdisk /dev/xvdb
-sudo mount -a
-sudo chmod 777 /mnt
-
-
-
-#// TBD for current image: sudo resize2fs /dev/nvme1n1
-
-
-
-
-### PLOT 
-
-tmux new -s plot
-
-cd /home/ubuntu/chia-blockchain/
 . ./activate
 
-
-# Threads in parallel
-#time chia plots create -k 32 -b 5120 -r 2 -u 128 -n 5 -t /tmpdisk -d /tmpdisk -f a4ab1f8213ebc06cbce49bf56b923467cf3263fdf587efff5ef0b71635f7c5b51d239046586354db24dfab188071d978 -p 96cbc06eb710bb3c43e8a64208dd49bce37cf440ca1c2bd6ee3c99124818c8ad0520d0b3f401151148a3a0e3c97f9f03
-
-#SET & Collect some info to be used later 
-PLOT_BYTES=4096
-PLOT_THREADS=4
-PLOTS_IN_PARALLEL=6
-PLOT_STAGGER_TIME=20
-PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-LOCAL_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/local-hostname`
-INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
-INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
-DATE_PREFIX=`date '+%Y-%m-%d_%H-%M-%S-%N'`
-PLOT_LOGFILE="/mnt/plotlogs/${DATE_PREFIX}_${INSTANCE_ID}"
-
-###DEBUG
-echo "PUBLIC_IP: $PUBLIC_IP
-LOCAL_HOSTNAME: $LOCAL_HOSTNAME
-INSTANCE_TYPE: $INSTANCE_TYPE
-INSTANCE_ID: $INSTANCE_ID
-PLOT_BYTES: $PLOT_BYTES
-PLOT_THREADS: $PLOT_THREADS
-DATE_PREFIX: $DATE_PREFIX
-PLOT_LOGFILE: $PLOT_LOGFILE"
+chia init
 
 
-#Create Log file w/Header
-echo "################ PLOT KING 9000 ###################
-PUBLIC_IP: $PUBLIC_IP
-LOCAL_HOSTNAME: $LOCAL_HOSTNAME
-INSTANCE_TYPE: $INSTANCE_TYPE
-INSTANCE_ID: $INSTANCE_ID
+# INSTALL & CONFIG PLOTMAN 
+sudo mkdir -p /home/chia/chia/logs
+sudo chmod 777 /home/chia/chia/logs
 
-" > $PLOT_LOGFILE
+pip install --force-reinstall git+https://github.com/ericaltendorf/plotman@main
 
-#One thread, plenty of overhead w/leftover GB's
-# IS TMPDISK THE RIGHT OUTPUT?
-time chia plots create -k 32 -b $PLOT_BYTES -r $PLOT_THREADS -u 128 -n 1 -t /mnt -d /mnt -f a4ab1f8213ebc06cbce49bf56b923467cf3263fdf587efff5ef0b71635f7c5b51d239046586354db24dfab188071d978 -p 96cbc06eb710bb3c43e8a64208dd49bce37cf440ca1c2bd6ee3c99124818c8ad0520d0b3f401151148a3a0e3c97f9f03;echo "**************** PLOTTING COMPLETE ****************" >> $PLOT_LOGFILE;aws s3 mv /mnt s3://lotsoplots --recursive --exclude "*" --include "*.plot" --exclude "*/*"; aws s3 cp $PLOT_LOGFILE s3:/lotsoplots/plotlogs/ ; echo "CHECK for processes and /mnt dir for plots before shutting down!"
+######### CONFIG FILE 
+#plotman config generate
+mkdir -p /home/ubuntu/.config/plotman/
+cp ~/chia-blockchain/utils/plotman.yaml /home/ubuntu/.config/plotman/plotman.yaml
 
-#TBD: rm -rf /tmpdisk???? - can't remove all because it's too large, what's left???
-
-### No need to spend money on the instnace if done plotting
-sudo shutdown
-
-
-
-
+## Other goodies 
+sudo apt install nmon awscli -y
+aws configure
+	######################################### 
+	# AWS Access Key ID [None]: *********
+	# AWS Secret Access Key [None]: *********
+	# Default region name [None]: us-west-2
+	# Default output format [None]: text
 
 
+# Setup directories
+sudo mkdir -p /mnt
+sudo chmod 777 /mnt
+mkdir /mnt/d1
+mkdir /mnt/d2
+mkdir /mnt/d3
+mkdir /mnt/d4
+mkdir /mnt/d5
+mkdir /mnt/d6
+mkdir /mnt/d7
+mkdir /mnt/d8
+
+sudo mkfs -t btrfs /dev/nvme0n1
+sudo mount /dev/nvme0n1 /mnt/d1
+
+sudo mkfs -t btrfs /dev/nvme1n1
+sudo mount /dev/nvme1n1 /mnt/d1
+
+sudo mkfs -t btrfs /dev/nvme2n1
+sudo mount /dev/nvme2n1 /mnt/d2
+
+sudo mkfs -t btrfs /dev/nvme3n1
+sudo mount /dev/nvme3n1 /mnt/d3
+
+sudo mkfs -t btrfs /dev/nvme4n1
+sudo mount /dev/nvme4n1 /mnt/d4
+
+sudo mkfs -t btrfs /dev/nvme5n1
+sudo mount /dev/nvme5n1 /mnt/d5
+
+sudo mkfs -t btrfs /dev/nvme6n1
+sudo mount /dev/nvme6n1 /mnt/d6
+
+sudo mkfs -t btrfs /dev/nvme7n1
+sudo mount /dev/nvme7n1 /mnt/d7
+
+sudo mkfs -t btrfs /dev/nvme8n1
+sudo mount /dev/nvme8n1 /mnt/d8
+
+sudo chmod 777 /mnt/* 
 
 
 
+
+
+
+
+### LETS GO!
+tmux new -t plotwatch
+nohup utils/plotwatcher.sh &
+
+tmux new -t plotman
+. ~/chia-blockchain/activate
+plotman interactive
